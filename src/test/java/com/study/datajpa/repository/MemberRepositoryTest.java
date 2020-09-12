10a77192f771ef57e473e6d81b6a192c9bf8cce8
junghun5947 <junghun5947@gmail.com>
2020-09-12 22:27:17 +0900

    member spring data jpa test case 추가 검증

--- src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -28,4 +30,40 @@ class MemberRepositoryTest {
         assertThat(findMember).isEqualTo(member);
     }
 
+
+
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        /*변경 감지 (Dirty checking)
+         * Entity 조회시 해당 엔티티의 조회상태 그대로 스냅샷을 만듬
+         * transaction이 끝나는 시점 스냅샷과 비교하여 update query 전달 -> 영속성 컨텍스트가 관리하는 엔티티에만 적용
+         * 전체 필드 업데이트하므로 특정 필드만 update시에는 Entity에 @DynamicUpdate
+         */
+        //findMember1.setUsername("PJH!!!!!");
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 카운트 검증
+        Long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        Long deleteCount = memberRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
+    }
 }
