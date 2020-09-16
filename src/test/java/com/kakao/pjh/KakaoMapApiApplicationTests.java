68ff5f595036c8cf74299e10199723fc344e6be7
Park JungHoon <junghun5947@gmail.com>
2020-09-16 18:14:14 +0900

    Initial commit

---
+++ src/test/java/com/kakao/pjh/KakaoMapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.pjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoMapApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
