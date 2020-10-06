9ff4aa88d1566a95b1fb1b0709747ab938180bf2
junghun5947 <junghun5947@gmail.com>
2020-10-07 00:28:30 +0900

    member column 매핑

--- src/main/java/com/study/jpa/hibernate/domain/Member.java
+++ src/main/java/com/study/jpa/hibernate/domain/Member.java
@@ -1,16 +1,47 @@
 package com.study.jpa.hibernate.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
-@Entity
+@Entity(name = "HibernateMember")
 @Getter
 @Setter
+@Table(name = "TBL_MEMBER")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id
     private Long id;
+
+    @Column(name = "name"
+            , insertable = true
+            , updatable = true
+            , nullable = false
+    ,columnDefinition = "blah")
     private String name;
+
+    private Integer age;
+
+    //EnumType Ordinal 은 지양
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createDate;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+    private LocalDate createDate;
+    private LocalDateTime lastModifiedDate;
+
+    @Lob //large content
+    private String description;
+
+    @Transient //not use
+    private int temp;
 }
