cb96c2136e5e18c9932681c0e0c8296b3653b47c
junghun5947 <junghun5947@gmail.com>
2020-10-07 22:04:29 +0900

    id 식별자 전략 강의 https://www.inflearn.com/course/ORM-JPA-Basic/lecture/21694?tab=curriculum

--- src/main/java/com/study/jpa/hibernate/domain/Member.java
+++ src/main/java/com/study/jpa/hibernate/domain/Member.java
@@ -20,7 +20,7 @@ import java.util.Date;
         sequenceName = "MEMBER_SEQ",
         initialValue = 1, allocationSize = 1)
 //initialValue 시작
-//allocationSize 증가값
+//allocationSize 증가값 -> 50으로 설정하여 네트워크 부하를 줄일 수 있음(미리 가져온 sequence 값으로 memory에서만 호출함) 안쓰면 낭비 주의
 public class Member {
     @Id
 //    @GeneratedValue(strategy = GenerationType.AUTO) // db 방언에 맞춰 실행
@@ -29,7 +29,8 @@ public class Member {
         기본키 생성을 dbms에 위임 ex. mysql = auto_increment
         Identity 전략을 사용하는 entity에 persistent를 하게되면 key를 얻어와야하기때문에 트랜잭션 중간에 insert query가 query 저장소에서 시작된다다
     */
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE) // 기본 sequence 생성하여 call next value
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE) // 기본 sequence 생성하여 call next value,
+    // 영속성 컨텍스트에는 항상 key가 있어야하기때문에 시퀀스 전략일때 persist 호출시 sequence call next를 먼저 한다.
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
