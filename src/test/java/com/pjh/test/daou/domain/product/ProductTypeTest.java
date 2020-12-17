f7946dac8394d245294c9afceac43000268c3cd5
junghun5947 <junghun5947@gmail.com>
2020-12-18 01:55:13 +0900

    controller, service 등 test case 추가

--- src/test/java/com/pjh/test/daou/domain/product/ProductTypeTest.java
+++
@@ -1,8 +0,0 @@
-package com.pjh.test.daou.domain.product;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ProductTypeTest {
-    //TODO
-
-}
--- src/test/java/com/pjh/test/daou/service/ProductOrderServiceTest.java
+++ src/test/java/com/pjh/test/daou/service/ProductOrderServiceTest.java
@@ -1,18 +1,17 @@
 package com.pjh.test.daou.service;
 
-import com.pjh.test.daou.domain.Delivery;
+import com.pjh.test.daou.controller.form.TradeForm;
 import com.pjh.test.daou.domain.ProductMaster;
 import com.pjh.test.daou.domain.ProductTrade;
-import com.pjh.test.daou.domain.RecipientInfo;
-import com.pjh.test.daou.domain.enumerate.DeliveryStatus;
 import com.pjh.test.daou.domain.enumerate.OrderStatus;
-import com.pjh.test.daou.domain.product.Outer;
-import com.pjh.test.daou.exception.NotEnoughStockException;
-import com.pjh.test.daou.exception.NotFoundProductException;
+import com.pjh.test.daou.repository.ProductMasterRepository;
 import com.pjh.test.daou.repository.ProductTradeRepository;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -21,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
@@ -35,76 +35,79 @@ class ProductOrderServiceTest {
     ProductOrderService productOrderService;
     @Autowired
     ProductTradeRepository productTradeRepository;
+    @Autowired
+    ProductMasterRepository productMasterRepository;
 
-    ProductMaster outer;
-    Delivery delivery;
-    int orderCount;
-
-    @BeforeEach
-    public void setup(){
-        outer = createClothes("Cardigan", 15000, 5);
-        delivery = createDelivery("박정훈", "01027075947", "경기도 용인시 수지구 죽전동 454");
-        orderCount = 2;
+    public void productSetup(){
+        productMasterRepository.deleteAll();
+        ProductMaster productMaster = new ProductMaster();
+        productMaster.setId(1L);
+        productMaster.setName("테스트 상품");
+        productMaster.setStock(5);
+        productMaster.setPrice(15000);
+        productMasterRepository.save(productMaster);
     }
 
-    /**
-     * ================================================= 주문 정상 조회 테스트
-     */
-    @Test
-    public void productTradeTest() {
-        //given
-        orderCount = 2;
+    private static Stream<Arguments> setTradeForm(){
+
+        return Stream.of(
+                Arguments.of(1, 15000, 4),
+                Arguments.of(2, 30000, 3),
+                Arguments.of(3, 45000, 2),
+                Arguments.of(4, 60000, 1),
+                Arguments.of(5, 75000, 0)
+        );
+    }
+    // 주문 정상 조회 테스트 실DB 연동
+    @Disabled
+    @ParameterizedTest
+    @MethodSource("setTradeForm")
+    public void productTradeTest(int orderCount, int totalPrice, int stock) {
+//        productSetup();
+        productMasterRepository.deleteAll();
+        ProductMaster productMaster = new ProductMaster();
+        productMaster.setId(1L);
+        productMaster.setName("테스트 상품");
+        productMaster.setStock(5);
+        productMaster.setPrice(15000);
+        productMasterRepository.save(productMaster);
+
+        TradeForm tradeForm = new TradeForm();
+        tradeForm.setOrderCount(orderCount);
 
         //when
-        Long tradeId = productOrderService.trade(outer.getId(), delivery, orderCount);
+        Long tradeId = productOrderService.trade(1L, tradeForm);
         Optional<ProductTrade> productTrade = productTradeRepository.findById(tradeId);
+        Optional<ProductMaster> productMasterO = productMasterRepository.findById(1L);
 
         //then
         assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, productTrade.get().getStatus());
-        assertEquals("주문한 상품 종류 수가 정확해야 한다.",1, productTrade.get().getOrderLines().size());
-        assertEquals("주문 가격은 가격 * 수량이다.", 15000 * 2, productTrade.get().getTotalPrice());
-        assertEquals("주문 수량만큼 재고가 줄어야 한다.",3, outer.getStock());
+        assertEquals("주문한 상품 종류 수가 정확해야 한다.",orderCount, productTrade.get().getOrderLines().size());
+        assertEquals("주문 가격은 가격 * 수량이다.", totalPrice, productTrade.get().getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야 한다.",stock, productMasterO.get().getStock());
     }
 
-    /**
-     * ================================================= 주문시 제품의 재고가 초과일때
-     */
+    // 주문시 제품의 재고가 초과일때
     @Test
     public void productStockOverTest() {
         //given
-        orderCount = 10;
+        productSetup();
+        int orderCount = 10;
+        TradeForm tradeForm = new TradeForm();
+        tradeForm.setOrderCount(orderCount);
+
         //when then
-        Long tradeId = productOrderService.trade(outer.getId(), delivery, orderCount);
-        Optional<ProductTrade> productTrade = productTradeRepository.findById(tradeId);
-        assertThrows(NotEnoughStockException.class, () -> productOrderService.trade(productTrade.get().getId(), delivery, orderCount));
+        assertThrows(RuntimeException.class, () -> productOrderService.trade(1L, tradeForm));
     }
 
-    /**
-     * ================================================= 주문시 상품정보 찾을 수 없을 때
-     */
+    // 주문시 상품정보 찾을 수 없을 때
     @Test
     public void productNotfoundTest(){
         //given
-        orderCount = 2;
+        int orderCount = 2;
+        TradeForm tradeForm = new TradeForm();
+        tradeForm.setOrderCount(orderCount);
         //when then
-        assertThrows(NotFoundProductException.class, () -> productOrderService.trade(1000L, delivery, orderCount));
-    }
-
-    private Outer createClothes(String name, int price, int stockQuantity) {
-        Outer outer = new Outer();
-
-        outer.setName(name);
-        outer.setStock(stockQuantity);
-        outer.setPrice(price);
-        em.persist(outer);
-        return outer;
-    }
-
-    private Delivery createDelivery(String name, String phone, String address){
-        Delivery delivery = new Delivery();
-        RecipientInfo recipientInfo = new RecipientInfo(name, phone, address);
-        delivery.setRecipientInfo(recipientInfo);
-        delivery.setStatus(DeliveryStatus.READY);
-        return delivery;
+        assertThrows(RuntimeException.class, () -> productOrderService.trade(1000L, tradeForm ));
     }
 }
