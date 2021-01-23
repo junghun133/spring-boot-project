35f3425d2e131282411633a278474f20e0e6a2d0
pjh <junghun5947@gmail.com>
2021-01-24 00:42:37 +0900

    Initial commit

---
+++ src/test/java/com/pjh/cp/SpringBootCachePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjh.cp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCachePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
